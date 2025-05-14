32195ece8c47e5dabeca32855d7ee700cbd954e5
devhimanshuu <devhimanshuu@gmail.com>
2025-05-14 16:56:14 +0530

    Second Commit updated chat Entity

--- complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/entity/Chat.java
+++ complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/entity/Chat.java
@@ -1,96 +1,101 @@
 package com.capgemini.complaintsmanagementsystem.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "chat")
 public class Chat {
-
-
-	@Id
+    
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty("ChatID")
-	@Column(name = "ChatID")
-	@NotNull(message = "ChatID is required")
+    @Column(name = "chat_id")
     private Long chatId;
-
-	@JsonProperty("ComplaintID")
-	@Column(name = "ComplaintID")
-	@NotNull(message = "ComplaintID is required")
+    
+    @Column(name = "complaint_id")
     private Long complaintId;
-
-    private Boolean chatSender;     
-    private Boolean chatReceiver;  
+    
+    @Column(name = "chat_sender")
+    private String chatSender;
+    
+    @Column(name = "chat_receiver")
+    private String chatReceiver;
+    
+    @Column(name = "chat_message", columnDefinition = "TEXT")
     private String chatMessage;
-
+    
+    @Column(name = "chat_timestamp")
     private LocalDateTime chatTimestamp;
 
+	public Long getChatId() {
+		return chatId;
+	}
+
+	public void setChatId(Long chatId) {
+		this.chatId = chatId;
+	}
+
+	public Long getComplaintId() {
+		return complaintId;
+	}
+
+	public void setComplaintId(Long complaintId) {
+		this.complaintId = complaintId;
+	}
+
+	public String getChatSender() {
+		return chatSender;
+	}
+
+	public void setChatSender(String chatSender) {
+		this.chatSender = chatSender;
+	}
+
+	public String getChatReceiver() {
+		return chatReceiver;
+	}
+
+	public void setChatReceiver(String chatReceiver) {
+		this.chatReceiver = chatReceiver;
+	}
+
+	public String getChatMessage() {
+		return chatMessage;
+	}
+
+	public void setChatMessage(String chatMessage) {
+		this.chatMessage = chatMessage;
+	}
+
+	public LocalDateTime getChatTimestamp() {
+		return chatTimestamp;
+	}
+
+	public void setChatTimestamp(LocalDateTime chatTimestamp) {
+		this.chatTimestamp = chatTimestamp;
+	}
+
+	public Chat(Long chatId, Long complaintId, String chatSender, String chatReceiver, String chatMessage,
+			LocalDateTime chatTimestamp) {
+		super();
+		this.chatId = chatId;
+		this.complaintId = complaintId;
+		this.chatSender = chatSender;
+		this.chatReceiver = chatReceiver;
+		this.chatMessage = chatMessage;
+		this.chatTimestamp = chatTimestamp;
+	}
+
+	public Chat() {
+		super();
+	}
     
-    public Chat() {}
-
-    public Chat(Long complaintId, Boolean chatSender, Boolean chatReceiver,
-                       String chatMessage, LocalDateTime chatTimestamp) {
-        this.complaintId = complaintId;
-        this.chatSender = chatSender;
-        this.chatReceiver = chatReceiver;
-        this.chatMessage = chatMessage;
-        this.chatTimestamp = chatTimestamp;
-    }
-
-  
-    public Long getChatId() {
-        return chatId;
-    }
-
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
-
-    public Long getComplaintId() {
-        return complaintId;
-    }
-
-    public void setComplaintId(Long complaintId) {
-        this.complaintId = complaintId;
-    }
-
-    public Boolean getChatSender() {
-        return chatSender;
-    }
-
-    public void setChatSender(Boolean chatSender) {
-        this.chatSender = chatSender;
-    }
-
-    public Boolean getChatReceiver() {
-        return chatReceiver;
-    }
-
-    public void setChatReceiver(Boolean chatReceiver) {
-        this.chatReceiver = chatReceiver;
-    }
-
-    public String getChatMessage() {
-        return chatMessage;
-    }
-
-    public void setChatMessage(String chatMessage) {
-        this.chatMessage = chatMessage;
-    }
-
-    public LocalDateTime getChatTimestamp() {
-        return chatTimestamp;
-    }
-
-    public void setChatTimestamp(LocalDateTime chatTimestamp) {
-        this.chatTimestamp = chatTimestamp;
-    }
+	
+    
 }
