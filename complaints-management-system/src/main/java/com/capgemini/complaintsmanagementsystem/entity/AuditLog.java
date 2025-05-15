86998cc36c42859b6ee1875b1b1d50ed25242b4d
Shubham Prasad <shubhamprasad7777@gmail.com>
2025-05-15 12:05:57 +0530

    "Unit Testing"

--- complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/entity/AuditLog.java
+++ complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/entity/AuditLog.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "audit_log")
