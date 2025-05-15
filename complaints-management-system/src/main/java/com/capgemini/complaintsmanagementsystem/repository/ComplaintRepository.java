59f45aa79aa922fa5e19e30eee16d519a7160ebb
Paritosh <paritoshsaini3@gmail.com>
2025-05-15 17:04:17 +0530

    added dashboard contents

--- complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/repository/ComplaintRepository.java
+++ complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/repository/ComplaintRepository.java
@@ -2,6 +2,14 @@ package com.capgemini.complaintsmanagementsystem.repository;
 
 import com.capgemini.complaintsmanagementsystem.entity.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+
+    @Query("select c.complaintSeverity,count(cs.complaintId) as complaintCount from ComplaintType c left join Complaint cs on c.complaintTypeId = cs.complaintTypeId group by c.complaintTypeId,c.complaintSeverity order by complaintCount desc")
+    List<Object[]> countComplaintsBySeverity();
+
+
 }
