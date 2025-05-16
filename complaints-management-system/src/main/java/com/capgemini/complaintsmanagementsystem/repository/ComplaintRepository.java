6ae50cfbe552e646ed0070ed454659250bcdcd63
Atharv-Hule-33 <131991958+Atharv-Hule-33@users.noreply.github.com>
2025-05-16 11:37:05 +0530

    Pumang2 branch (#26)
    * ComplaintTypeTest
    
    * Added Integration Testing for Complaint Type
    
    * Added Exceptions for Complaint and Complaint Type
    
    * added logs
    
    * Added queries in department user and complaints
    
    * changed complaintType to complaintTypeName in ComplaintType Entity for
    Sonar
    
    ---------
    
    Co-authored-by: Pumang Kirange <kirangespumang03@gmail.com>

--- complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/repository/ComplaintRepository.java
+++ complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/repository/ComplaintRepository.java
@@ -12,18 +12,14 @@ import java.util.List;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
 
+
+
     @Query("select c.complaintSeverity,count(cs.complaintId) as complaintCount from ComplaintType c left join Complaint cs on c.complaintTypeId = cs.complaintTypeId group by c.complaintTypeId,c.complaintSeverity order by complaintCount desc")
     List<Object[]> countComplaintsBySeverity();
 
 
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
- 
-	@Query("SELECT c.status, COUNT(c) FROM Complaint c GROUP BY c.status")
+    @Query("SELECT c.complaintStatus, COUNT(c) FROM Complaint c GROUP BY c.complaintStatus")
 	List<Object[]> countComplaintsByStatus();
-
-
-	public Long countByComplaintId();
 
 
 }
