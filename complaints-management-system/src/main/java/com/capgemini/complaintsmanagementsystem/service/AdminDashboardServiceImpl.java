caa906047ba270ec39348bb12753c91aaa95f886
Atharv-Hule-33 <131991958+Atharv-Hule-33@users.noreply.github.com>
2025-05-16 18:49:39 +0530

    Paritosh branch (#31)
    * Delete .idea directory
    
    * 2nd commit from paritosh
    
    * 3rd commit
    
    * 3rd commit
    
    * added ComplaintNotfoundException
    
    * added logs
    
    * added logs
    
    * added dashboard contents
    
    * changed application.properties
    
    * dashboard Dto
    
    * frontend
    
    * Delete .idea directory
    
    extra idea file
    
    ---------
    
    Co-authored-by: Paritosh Saini <154421461+paritosh219@users.noreply.github.com>
    Co-authored-by: Paritosh <paritoshsaini3@gmail.com>

--- complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/service/AdminDashboardServiceImpl.java
+++ complaints-management-system/src/main/java/com/capgemini/complaintsmanagementsystem/service/AdminDashboardServiceImpl.java
@@ -1,14 +1,17 @@
 package com.capgemini.complaintsmanagementsystem.service;
 
 import com.capgemini.complaintsmanagementsystem.Dto.AdminDashboardDto;
-import com.capgemini.complaintsmanagementsystem.entity.Complaint;
+import com.capgemini.complaintsmanagementsystem.entity.ComplaintSeverity;
 import com.capgemini.complaintsmanagementsystem.repository.ComplaintRepository;
 import com.capgemini.complaintsmanagementsystem.repository.DepartmentRepository;
 import com.capgemini.complaintsmanagementsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
 
 @Service
 public class AdminDashboardServiceImpl implements AdminDashboardService{
@@ -26,6 +29,44 @@ public class AdminDashboardServiceImpl implements AdminDashboardService{
 
     @Override
     public List<AdminDashboardDto> getDashBoardData() {
-        return List.of();
+        List<AdminDashboardDto> dataList = new ArrayList<>();
+        AdminDashboardDto data = new AdminDashboardDto();
+        data.setNumberOfComplaints(complaintRepository.count());
+        data.setNumberOfDepartments(departmentRepository.count());
+        data.setNumberOfUsers(userRepository.count());
+        data.setClosedComplaints(complaintRepository.countClosedComplaints());
+        data.setInProgressComplaints(complaintRepository.countInProgressComplaints());
+        data.setFiledComplaints(complaintRepository.countFiledComplaints());
+        data.setResolvedComplaints(complaintRepository.countResolvedComplaints());
+        List<Object[]> complaintsBySeverity = complaintRepository.countComplaintsBySeverity();
+        Map<ComplaintSeverity,Long> complaintSeverityMap = new HashMap<>();
+        List<Object[]> complaintsByDate = complaintRepository.countComplaintsByDate();
+        Map<LocalDateTime,Long> complaintsByDateMap = new HashMap<>();
+        for(Object[] complaint : complaintsBySeverity){
+            ComplaintSeverity severity = (ComplaintSeverity) complaint[0];
+            Long complaintCount = (Long) complaint[1];
+            complaintSeverityMap.put(severity,complaintCount);
+        }
+        data.setComplaintsCountBySeverity(complaintSeverityMap);
+
+        for(Object[] complaint : complaintsByDate){
+            Object dateObject = complaint[0];
+            if (dateObject instanceof java.sql.Date) {
+                java.sql.Date sqlDate = (java.sql.Date) dateObject;
+
+                java.util.Date utilDate = new java.util.Date(sqlDate.getTime());
+                java.time.Instant instant = utilDate.toInstant();
+                java.time.LocalDateTime localDateTime = instant.atZone(java.time.ZoneId.systemDefault()).toLocalDateTime();
+
+                Long complaintCount = (Long) complaint[1];
+                complaintsByDateMap.put(localDateTime,complaintCount);
+            } else {
+                // Handle the case where the date is not a java.sql.Date
+                System.err.println("Unexpected date type: " + dateObject.getClass().getName());
+            }
+        }
+        data.setComplaintsCountByDate(complaintsByDateMap);
+        dataList.add(data);
+        return dataList;
     }
 }
